23104c86f5ba1a8213033800d4ff387b3e09a296
ln18520340281 <ln18520340281@163.com>
2023-03-01 21:44:20 +0800

    新增菜品！

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -1,13 +1,18 @@
 package com.itheima.reggie.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Dish;
 import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 
@@ -19,6 +24,18 @@ public class DishController {
 //	@Autowired
 //	private DishFlavorService dishFlavorService;
 
+	@GetMapping("/page")
+	public R<Page<Dish>> pageSearch(Integer page, Integer pageSize) {
+		// log.info("page={},pagesize={},name={}", page, pageSize, name);
+
+		LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<Dish>();
+		queryWrapper.orderByDesc(Dish::getSort);
+
+		Page<Dish> pageinfo = new Page<>(page, pageSize);
+		dishService.page(pageinfo, queryWrapper);
+		return R.success(pageinfo);
+	}
+	
 	/**
 	 * 新增菜品
 	 * 
